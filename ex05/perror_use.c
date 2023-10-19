b8fd6a8309dc6c2d25d515a08681e8551fd3f51c
sujiinnn <sujiinnn@gmail.com>
2023-10-19 12:07:35 +0900

    ex05_my_perror() 함수 구현

---
+++ ex05/perror_use.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+void my_perror(const char *str)
+{
+    fprintf(stderr, "%s : %s\n", str, strerror(errno));
+}
+
+void main(int argc, char *argv[])
+{
+    FILE *f;
+
+    if(argc < 2){
+        printf("Usage: perror_use file_name\n");
+        exit(1);
+    }
+
+    if((f = fopen(argv[1], "r")) == NULL) {
+        my_perror("fopen");
+        exit(1);
+    }
+
+    printf("Open a file \"%s\".\n", argv[1]);
+
+    fclose(f);
+}
